f562528450961a5f227c666e31437ace852517df
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2025-03-25 01:51:36 +0300

    urdf

--- examples/simulate.rs
+++ examples/simulate.rs
@@ -116,7 +116,7 @@ fn setup(
 
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0), //.looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
         FlyCam,
